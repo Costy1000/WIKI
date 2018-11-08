88cfabc6a0cf679e4da0d9d8a902da8c89b18d41
Mihai Marvel <costymihai320@gmail.com>
2018-11-09 00:13:42 +0200

    wiki

---
+++ script.js
@@ -0,0 +1,22 @@
+function linksFunction(){
+    location.href= "#story";
+    location.href= " #gameplay";
+    location.href=" #game-menu";
+    location.href=" #easter-egg";
+    location.href=" #references";
+    location.href=" #external-links";
+    location.href=" #Atari-Times";
+    location.href=" #Arcadians";
+    location.href=" #Atari";
+    location.href=" #James";
+    location.href=" #Federico";
+     location.href=" #last";
+} 
+
+    
+ 
+ 
+ 
+/*function gameFunction(){
+    location.href="#gameplay";
+}
